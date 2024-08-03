d16d7b5534d5ab97e04acc75eef88f18c86acacd
Joachim KPOGNON <koami.credo@gmail.com>
2024-08-03 19:06:27 +0200

    chore: add missing importation of task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config/app-config.module';
 import { DatabaseModule } from './database/database.module';
 import { ProjectModule } from './project-management/project/project.module';
+import { TaskModule } from './project-management/task/task.module';
 
 @Module({
-  imports: [AppConfigModule, DatabaseModule, ProjectModule],
+  imports: [AppConfigModule, DatabaseModule, ProjectModule, TaskModule],
   controllers: [],
   providers: [],
 })
