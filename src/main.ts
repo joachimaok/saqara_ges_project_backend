0d1499a7150c854b5e08019cafe4391250909563
Joachim KPOGNON <koami.credo@gmail.com>
2024-08-04 22:37:18 +0200

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,15 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  app.enableCors({
+    origin: '*', // TODO: consider removing it because it is not production ready
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('APP_PORT', 3000);
+
+  await app.listen(port);
 }
 bootstrap();
